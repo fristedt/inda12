bb56cc6ca100d66f0ca458a1b4940b950e1d2c97
Hampus Fristedt <Hampus Fristedt>
2013-03-20 21:00:16 +0100

    Hi again!

--- Clock.go
+++ Clock.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func Remind(text string, paus time.Duration) {
+	for {
+		fmt.Println("Klockan är " + time.Now().Format("15:04:05") + ": " + text)
+		time.Sleep(paus)
+	}
+}     
 
 func main() {
+	Remind("Hej!", time.Second)
+}
      
